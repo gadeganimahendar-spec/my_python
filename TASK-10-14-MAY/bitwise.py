ed6d91cc7e7f289c609a7e7a675462e5588089c6
gadeganimahendar-spec <gadeganimahendar@gmail.com>
2026-06-26 14:51:59 +0530

    bitwise

---
+++ TASK-10-14-MAY/bitwise.py
@@ -0,0 +1,25 @@
+# If two values are same values perform “XOR” operation  Perform “XOR” be if two values are same what will be the output.  Perform and operation 1 is even? Or odd? What will the output?  Practice all bitwise operator. 
+
+# bit wise operators
+x=10
+y=20
+print(x&y)
+print(x|y)
+print(x^y)
+print(~x)
+print(x<<2)
+print(x>>2)
+
+# given 1 is even or odd by using "XOR"
+n=1
+if n^1==n+1:
+    print("even")
+else :
+    print("odd")
+
+
+# performing the two operations are same
+a=10
+b=10
+n=a^b
+print(n)
