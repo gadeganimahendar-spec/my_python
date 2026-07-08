9d747b26035b64ae6190ce083e490601411cad8c
gadeganimahendar-spec <gadeganimahendar@gmail.com>
2026-07-08 15:58:46 +0530

    mulmatrix

--- TASK-17-30-MAY/complist.py
+++ TASK-17-30-MAY/complist.py
@@ -1,10 +1,16 @@
-# separate vowels and consonants in string "hi hello raju"
+# # separate vowels and consonants in string "hi hello raju"
 
 
-s = str(input("Enter the characters: "))
+# s = str(input("Enter the characters: "))
+# l1=[]
+# l2=[]
+# l = [ [ch for ch in s if ch.lower() in "aeiou"],[ch for ch in s if ch.isalpha() and ch.lower() not in "aeiou"]]
 
-l = [ [ch for ch in s if ch.lower() in "aeiou"],[ch for ch in s if ch.isalpha() and ch.lower() not in "aeiou"]]
+# print("Vowels:", l1)
+# print("Consonants:", l2)
 
-print("Vowels:", l[0])
-print("Consonants:", l[1])
 
+s= str(input("enter the charters"))
+l1=[]
+i2=[]
+l1=[ chr for chr in range ]
